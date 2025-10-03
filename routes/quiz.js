bc936573dbe68ccf1d1dc774e3ceab0e95382257
mohamed <mohamedboukab202@gmail.com>
2025-10-03 16:37:23 +0100

    Implement quiz list and start quiz functionality to render quiz data; add error handling and no-data views

--- routes/quiz.js
+++ routes/quiz.js
@@ -4,13 +4,14 @@ const QuizController = require('../controllers/QuizController');
 const authenticateToken = require('../middlewares/auth');
 const authorizeRoles = require('../middlewares/authorize');
 
+router.get('/list', authenticateToken, QuizController.getQuizList);
+router.get('/start/:quizType', authenticateToken, QuizController.startQuiz);
+
+
 // Get a quiz by theme
 router.get('/theme/:themeId', QuizController.getQuizByTheme);
 
 // Delete all questions by theme
-router.delete('/theme/:themeId', authenticateToken, authorizeRoles('admin'), QuizController.deleteQuiz);
-
-
-
+router.delete('/theme/:themeId', authenticateToken, authorizeRoles("admin"), QuizController.deleteQuiz );
 
 module.exports = router;
